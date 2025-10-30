ad570e81ae3f43ac3ca9b61363c2d0751adf0496
Lance Hudson <lancexhudson@gmail.com>
2025-10-29 22:26:11 -0400

    adding basename in index.js for public url

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const basename = process.env.PUBLIC_URL || '/overtimesportsapp';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
